a8e6ce24a714014c48871f70e8a8f651345853fc
cliffusion <cliffusion@users.noreply.github.com>
2018-07-05 21:48:55 +0800

    Update wp_setup.sh

--- wp_setup.sh
+++ wp_setup.sh
@@ -74,6 +74,7 @@ sed -i 's/password_here/password/g' wp-config.php
 echo "define('FS_METHOD', 'direct');" >> wp-config.php
 
 chown -R apache:apache ${WP_PATH}
+systemctl restart httpd.service
 echo "Wordpress configuration completed"
 echo ""
 sleep 1
@@ -81,17 +82,25 @@ sleep 1
 ### Complete Wordpress admin user setup ###
 echo "Setting Up WordPress user for the first time..."
 curl "http://127.0.0.1/wordpress/wp-admin/install.php?step=2" \
---data-urlencode "weblog_title=$WP_DOMAIN"\
---data-urlencode "user_name=$WP_ADMIN_USERNAME" \
---data-urlencode "admin_email=$WP_ADMIN_EMAIL" \
---data-urlencode "admin_password=$WP_ADMIN_PASSWORD" \
---data-urlencode "admin_password2=$WP_ADMIN_PASSWORD" \
+--data-urlencode "weblog_title=${WP_DOMAIN}" \
+--data-urlencode "user_name=${WP_ADMIN_USERNAME}" \
+--data-urlencode "admin_email=${WP_ADMIN_EMAIL}" \
+--data-urlencode "admin_password=${WP_ADMIN_PASSWORD}" \
+--data-urlencode "admin_password2=${WP_ADMIN_PASSWORD}" \
 --data-urlencode "pw_weak=1"
 echo "Wordpress Account Setup Completed"
 echo ""
 sleep 1
 
 ### Validate the wordpress page ###
-curl -isk http://$WP_DOMAIN/wordpress/
+curl -isk http://127.0.0.1/wordpress/
+
+### To further Edit these are the suggestions for improvements:
+### - Do a search at /var/www/html/wordpress and search 127.0.0.1
+### and perform a sed replace for 127.0.0.1 to your desired apache IP or WP_Domain Name
+### - Apache config should also startup to listen only on your desired IP
+### - MariaDB should also be harden after setup completes
+### - Apache should also be harden to allow only required core modules to startup
+### such as mod_host, mod_rewrite, and others that wordpress may required
 
 echo "Wordpress Setup Completed"
